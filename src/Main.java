0fb0ba5923fd97c0cc2174aa34c2a5a8429485e3
AnastaL <anastasia_los@mail.ru>
2024-11-22 18:07:06 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2700;
+        int rubForMile = 20;
+        int bonus = (ticket / rubForMile);
+        System.out.println(bonus);
+    }
+}
